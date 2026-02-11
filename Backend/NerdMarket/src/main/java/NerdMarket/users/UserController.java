9893a1102de5a8f9330efcb03f7284290b8a5908
34coopatroopa <cjhoy@iastate.edu>
2026-02-11 14:48:00 -0600

    Initial Project Repository

---
+++ Backend/NerdMarket/src/main/java/NerdMarket/users/UserController.java
@@ -0,0 +1,12 @@
+package NerdMarket.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+}
---
+++ Backend/NerdMarket/src/main/java/NerdMarket/users/Users.java
@@ -0,0 +1,14 @@
+package NerdMarket.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+}
