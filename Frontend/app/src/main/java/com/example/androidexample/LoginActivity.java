fcfd0461bd94edf9cec925f10fe692c1ee9a0fd1
Logan Heileman <logheil@iastate.edu>
2026-02-22 16:47:38 -0600

    Login and signup GUI

--- Frontend/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -12,7 +12,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
-    private Button loginButton;         // define login button variable
+    private Button goButton;         // define login button variable
     private Button signupButton;        // define signup button variable
 
     @Override
@@ -21,36 +21,39 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);            // link to Login activity XML
 
         /* initialize UI elements */
-        usernameEditText = findViewById(R.id.login_username_edt);
-        passwordEditText = findViewById(R.id.login_password_edt);
+        usernameEditText = findViewById(R.id.login_username);
+        passwordEditText = findViewById(R.id.login_password);
+        goButton = findViewById(R.id.login_go_btn);
+        signupButton = findViewById(R.id.login_signup_btn);
+
 
         /* click listener on login button pressed */
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                /* grab strings from user inputs */
-//                String username = usernameEditText.getText().toString();
-//                String password = passwordEditText.getText().toString();
-//
-//                /* when login button is pressed, use intent to switch to Login Activity */
-//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
-//                intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
-//                startActivity(intent);  // go to MainActivity with the key-value data
-//            }
-//        });
-//
-//        /* click listener on signup button pressed */
-//        signupButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                /* when signup button is pressed, use intent to switch to Signup Activity */
-//                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-//                startActivity(intent);  // go to SignupActivity
-//            }
-//        });
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* grab strings from user inputs */
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
+                intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
+                startActivity(intent);  // go to MainActivity with the key-value data
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when signup button is pressed, use intent to switch to Signup Activity */
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);  // go to SignupActivity
+            }
+        });
 
     }
 }
--- Frontend/app/src/main/java/com/example/androidexample/SignupActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -13,9 +13,12 @@ public class SignupActivity extends AppCompatActivity {
 
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
-    private EditText confirmEditText;   // define confirm edittext variable
-    private Button loginButton;         // define login button variable
-    private Button signupButton;        // define signup button variable
+    private EditText firstnameEditText;   // define confirm edittext variable
+    private EditText lastnameEditText;
+    private EditText emailEditText;
+
+    private Button createAccountButton;         // define login button variable
+    private Button backToLoginButton;        // define signup button variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,16 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         /* initialize UI elements */
-        usernameEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
-        passwordEditText = findViewById(R.id.signup_password_edt);  // link to password edtext in the Signup activity XML
-        confirmEditText = findViewById(R.id.signup_confirm_edt);    // link to confirm edtext in the Signup activity XML
-        loginButton = findViewById(R.id.signup_login_btn);    // link to login button in the Signup activity XML
-        signupButton = findViewById(R.id.signup_signup_btn);  // link to signup button in the Signup activity XML
+        usernameEditText = findViewById(R.id.signup_username);  // link to username edtext in the Signup activity XML
+        passwordEditText = findViewById(R.id.signup_password);  // link to password edtext in the Signup activity XML
+        firstnameEditText = findViewById(R.id.signup_firstname);    // link to confirm edtext in the Signup activity XML
+        lastnameEditText = findViewById(R.id.signup_lastname);
+        emailEditText = findViewById(R.id.signup_email);
+        createAccountButton = findViewById(R.id.create_account_btn);    // link to login button in the Signup activity XML
+        backToLoginButton = findViewById(R.id.back_to_login_btn);  // link to signup button in the Signup activity XML
 
         /* click listener on login button pressed */
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        backToLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -41,33 +46,32 @@ public class SignupActivity extends AppCompatActivity {
         });
 
         /* click listener on signup button pressed */
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 /* grab strings from user inputs */
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-                String confirm = confirmEditText.getText().toString();
 
                 /* Checking if the username and password are valid */
-                if (username.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Username is empty", Toast.LENGTH_LONG).show();
-                } else if (password.isEmpty() || confirm.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Password is empty", Toast.LENGTH_LONG).show();
-                }
-                else if (!password.equals(confirm)){
-                    Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
-                }
-                else if (password.length() < 8){
-                    Toast.makeText(getApplicationContext(), "Password must be 8+ characters", Toast.LENGTH_LONG).show();
-                }
-                else if (!password.matches(".*[^a-zA-Z0-9].*")){
-                    Toast.makeText(getApplicationContext(), "Password must contain a symbol", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
-                }
+//                if (username.isEmpty()){
+//                    Toast.makeText(getApplicationContext(), "Username is empty", Toast.LENGTH_LONG).show();
+//                } else if (password.isEmpty() || confirm.isEmpty()){
+//                    Toast.makeText(getApplicationContext(), "Password is empty", Toast.LENGTH_LONG).show();
+//                }
+//                else if (!password.equals(confirm)){
+//                    Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
+//                }
+//                else if (password.length() < 8){
+//                    Toast.makeText(getApplicationContext(), "Password must be 8+ characters", Toast.LENGTH_LONG).show();
+//                }
+//                else if (!password.matches(".*[^a-zA-Z0-9].*")){
+//                    Toast.makeText(getApplicationContext(), "Password must contain a symbol", Toast.LENGTH_LONG).show();
+//                }
+//                else{
+//                    Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+//                }
             }
         });
     }
