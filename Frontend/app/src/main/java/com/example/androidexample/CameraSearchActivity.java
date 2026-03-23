cc2a14ea4ed08bf7fcd67d62f679584cb1a49cb2
Logan Heileman <logheil@iastate.edu>
2026-03-23 13:05:28 -0500

    Feat: camera UI in progress

---
+++ Frontend/app/src/main/java/com/example/androidexample/CameraSearchActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+
+public class CameraSearchActivity extends AppCompatActivity {
+
+}
