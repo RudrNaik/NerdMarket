8672bbff003b18c9676869a64eb24ed4653367cf
Logan Heileman <logheil@iastate.edu>
2026-04-07 22:07:27 -0500

    Feat: Biggest movers full functionality

--- Frontend/app/src/main/java/com/example/androidexample/MoversActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/MoversActivity.java
@@ -2,12 +2,14 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +18,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,7 +27,7 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BiggestMoversActivity extends AppCompatActivity {
+public class MoversActivity extends AppCompatActivity {
     private LinearLayout cardContainer;
     private int id;
     private boolean isAdmin;
@@ -33,7 +36,7 @@ public class BiggestMoversActivity extends AppCompatActivity {
     private Button listPokemon;
     private Button listMTG;
     private Button listYugioh;
-    private Button homeButton;
+    private ImageView homeButton;
     private static final String BASE_URL = "http://coms-3090-022.class.las.iastate.edu:8080/api/prices/biggest-movers/type/";
 
     @Override
@@ -55,7 +58,10 @@ public class BiggestMoversActivity extends AppCompatActivity {
         }
 
         homeButton.setOnClickListener(v -> {
-            Intent intent = new Intent(BiggestMoversActivity.this, MainActivity.class);
+            Intent intent = new Intent(MoversActivity.this, MainActivity.class);
+            intent.putExtra("id", id);
+            intent.putExtra("isAdmin", isAdmin);
+            intent.putExtra("username", username);
             startActivity(intent);
         });
         listPokemon.setOnClickListener(v -> {
@@ -70,7 +76,7 @@ public class BiggestMoversActivity extends AppCompatActivity {
     }
 
     void getBiggestMovers(String game){
-        String url = BASE_URL + game + "/2days";
+        String url = BASE_URL + game + "/7days";
 
         JsonArrayRequest request = new JsonArrayRequest(
                 Request.Method.GET,
@@ -106,19 +112,31 @@ public class BiggestMoversActivity extends AppCompatActivity {
 
             String cardId = String.valueOf(card.getInt("cardId"));
             String changePercent = String.valueOf(card.getDouble("changePercent"));
+            double changePercentForTextColor = card.getDouble("changePercent");
 
             View inflated = LayoutInflater.from(this).inflate(R.layout.activity_biggest_movers, null, false);
 
             LinearLayout row = inflated.findViewById(R.id.biggestmovers_row_template);
             ((ViewGroup) row.getParent()).removeView(row);
 
+            TextView changeText = row.findViewById(R.id.biggestmovers_cardChange_txt);
+            changeText.setText(changePercent + "%");
+         //   ((TextView) row.findViewById(R.id.biggestmovers_cardChange_txt)).setText(changePercent + "%");
+            if (changePercentForTextColor >= 0){
+                changeText.setTextColor(Color.GREEN);
+            } else {
+                changeText.setTextColor(Color.RED);
+            }
+
             ((TextView) row.findViewById(R.id.biggestmovers_cardName_txt)).setText("Loading...");
-            ((TextView) row.findViewById(R.id.biggestmovers_cardChange_txt)).setText(changePercent + "%");
             row.setVisibility(View.VISIBLE);
 
             row.setOnClickListener(v -> {
-                Intent intent = new Intent(BiggestMoversActivity.this, CardSearchActivity.class);
+                Intent intent = new Intent(MoversActivity.this, CardSearchActivity.class);
                 intent.putExtra("bundleCardID", Integer.parseInt(cardId));
+                intent.putExtra("id", id);
+                intent.putExtra("isAdmin", isAdmin);
+                intent.putExtra("username", username);
                 startActivity(intent);
             });
 
@@ -127,7 +145,33 @@ public class BiggestMoversActivity extends AppCompatActivity {
         }
     }
     void getCardName(String cardID, View view){
+        String url = "http://coms-3090-022.class.las.iastate.edu:8080/api/cards/" + cardID;
 
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String name = "Unknown";
+                        try {
+                            name = response.getString("cardName");
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        ((TextView) view.findViewById(R.id.biggestmovers_cardName_txt)).setText(name);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("BiggestMovers", error.toString());
+                    }
+                }
+        ) {
+            @Override public Map<String, String> getHeaders() throws AuthFailureError { return new HashMap<>(); }
+        };
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
-
 }
